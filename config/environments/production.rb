dd9329c72c0ad5c30195baa8273c189675592596
Ari Cohen <arilovesrugby@gmail.com>
2015-02-23 14:52:20 -0500

    heroku setup with devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Reguired for Heroku.
+  # Note set to host orphanboy
+  config.action_mailer.default_url_options = { host: 'orphanboy.herokuapp.com' }
 end
